528e9c554fd7b215cd3bbf0df4cfcd30360e5864
Preston Hales <phales@trivir.com>
2025-06-23 08:35:58 -0600

    Add ability to export/import the ESV secret store in cloud deployments

--- src/ops/ConfigOps.ts
+++ src/ops/ConfigOps.ts
@@ -55,11 +55,6 @@ import {
   importCirclesOfTrust,
 } from './CirclesOfTrustOps';
 import {
-  exportSecretStores,
-  importSecretStores,
-  SecretStoreExportSkeleton,
-} from './classic/SecretStoreOps';
-import {
   exportServers,
   importServers,
   ServerExportInterface,
@@ -110,6 +105,11 @@ import {
   importScriptTypes,
   ScriptTypeExportSkeleton,
 } from './ScriptTypeOps';
+import {
+  exportSecretStores,
+  importSecretStores,
+  SecretStoreExportSkeleton,
+} from './SecretStoreOps';
 import { exportServices, importServices } from './ServiceOps';
 import { exportThemes, importThemes, ThemeSkeleton } from './ThemeOps';
 
@@ -683,7 +683,7 @@ export async function exportFullConfiguration({
             realmStateObj,
             'Secret Stores',
             resultCallback,
-            isClassicDeployment
+            isClassicDeployment || isCloudDeployment
           )
         )?.secretstore,
         service: (
@@ -1052,7 +1052,8 @@ export async function importFullConfiguration({
         indicatorId,
         'Secret Stores',
         resultCallback,
-        isClassicDeployment && !!importData.realm[realm].secretstore
+        (isClassicDeployment || isCloudDeployment) &&
+          !!importData.realm[realm].secretstore
       )
     );
     response.push(
