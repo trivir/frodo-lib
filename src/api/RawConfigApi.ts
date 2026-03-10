ceec2ef8b47f72b20678b056157e1f703c1ddc38
Dallin Sevy <dsevy@trivir.com>
2026-03-10 10:16:15 -0600

    Add putRaw functions to RawConfigApi and importRawConfig to RawConfigOps

--- src/api/RawConfigApi.ts
+++ src/api/RawConfigApi.ts
@@ -3,6 +3,7 @@ import util from 'util';
 import { State } from '../shared/State';
 import { getHostOnlyUrl, getIdmBaseUrl } from '../utils/ForgeRockUtils';
 import { generateAmApi, generateEnvApi, generateIdmApi } from './BaseApi';
+import { IdObjectSkeletonInterface } from './ApiTypes';
 
 const amTemplate: string = '%s/%s';
 const idmTemplate: string = '%s/%s';
@@ -90,6 +91,82 @@ export async function getRawEnv({
     resource: getApiConfig(apiVersion),
     state,
   }).get(urlString, { withCredentials: true });
+
+  return data;
+}
+
+/**
+ * Performs a put request against the specified AM endpoint
+ * @param {string} endpoint The AM endpoint (e.g. if full URL is https://<tenant-host>/am/<endpoint>, <endpoint> is the value to pass in)
+ * @param {ApiVersion} apiVersion The API version to use. Defaults to 2.0 for protocol and 1.0 for resource.
+ * @returns {Promise<any>} The response data from the endpoint
+ */
+export async function putRawAm({
+  endpoint,
+  payload,
+  apiVersion,
+  state,
+}: {
+  endpoint: string;
+  payload: IdObjectSkeletonInterface;
+  apiVersion?: ApiVersion;
+  state: State;
+}): Promise<any> {
+  const urlString = util.format(amTemplate, state.getHost(), endpoint);
+  const { data } = await generateAmApi({
+    resource: getApiConfig(apiVersion),
+    state,
+  }).put(urlString, payload, { withCredentials: true });
+
+  return data;
+}
+
+/**
+ * Performs a put request against the specified IDM endpoint
+ * @param {string} endpoint The IDM endpoint (e.g. if full URL is https://<tenant-host>/openidm/<endpoint>, <endpoint> is the value to pass in)
+ * @returns {Promise<any>} The response data from the endpoint
+ */
+export async function putRawIdm({
+  endpoint,
+  payload,
+  state,
+}: {
+  endpoint: string;
+  payload: IdObjectSkeletonInterface;
+  state: State;
+}): Promise<any> {
+  const urlString = util.format(idmTemplate, getIdmBaseUrl(state), endpoint);
+  const { data } = await generateIdmApi({ state }).put(urlString, payload);
+
+  return data;
+}
+
+/**
+ * Performs a put request against the specified Environment endpoint
+ * @param {string} endpoint The Environment endpoint (e.g. if full URL is https://<tenant-host>/environment/<endpoint>, <endpoint> is the value to pass in)
+ * @param {ApiVersion} apiVersion The API version to use. Defaults to 2.0 for protocol and 1.0 for resource.
+ * @returns {Promise<any>} The response data from the endpoint
+ */
+export async function putRawEnv({
+  endpoint,
+  payload,
+  apiVersion,
+  state,
+}: {
+  endpoint: string;
+  payload: IdObjectSkeletonInterface;
+  apiVersion?: ApiVersion;
+  state: State;
+}): Promise<any> {
+  const urlString = util.format(
+    envTemplate,
+    getHostOnlyUrl(state.getHost()),
+    endpoint
+  );
+  const { data } = await generateEnvApi({
+    resource: getApiConfig(apiVersion),
+    state,
+  }).put(urlString, payload, { withCredentials: true });
 
   return data;
 }
